2de15e30f9fea9cfcb991f5cc382df741ec7d63f
Felipe SMS <felipe.santana1995@hotmail.com>
2024-02-19 09:39:38 -0300

    Incluido jackson-databind no pom e aplicando no código.

--- src/main/java/br/com/felipe/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/felipe/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,27 @@
 package br.com.felipe.screenmatch;
 
+import br.com.felipe.screenmatch.model.DadosSerie;
+import br.com.felipe.screenmatch.service.ConsumeAPI;
+import br.com.felipe.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumeAPI consumeAPI = new ConsumeAPI();
+		var json = consumeAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=8b1809b5");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 
 }
